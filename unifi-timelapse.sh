35d96dbd039d3ed636ff13d1a19b9c0a1666db75
Doug Saus <doug.saus@yahoo.com>
2022-03-05 15:23:29 -0600

    Update unifi-timelapse.sh

--- unifi-timelapse.sh
+++ unifi-timelapse.sh
@@ -7,10 +7,7 @@ DATE_EXT=`date '+%F %H:%M'`
 
 declare -A CAMS
 
-CAMS["Front Door"]="http://192.1.1.1/snap.jpeg"
-CAMS["Back Door"]="http://192.1.1.2/snap.jpeg"
-CAMS["Driveway"]="http://192.1.1.3/snap.jpeg"
-CAMS["Back Garden"]="http://192.1.1.4/snap.jpeg"
+CAMS["Backyard"]="http://192.168.1.136/snap.jpeg"
 
 # If we are in a terminal, be verbose.
 if [[ -z $VERBOSE && -t 1 ]]; then
